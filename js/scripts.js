0d21cb859ca38fa746c6e2bede866adc7b9cff3f
lc\melof <95fedemelo@gmail.com>
2025-04-02 05:10:24 -0300

    Added buttons

--- js/scripts.js
+++ js/scripts.js
@@ -10,3 +10,40 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const scrollIcon = document.getElementById("scrollIcon");
+    
+    if (scrollIcon) {
+        scrollIcon.addEventListener("click", function (e) {
+            e.preventDefault();
+            const target = document.querySelector("#about");
+
+            if (target) {
+                window.scrollTo({
+                    top: target.offsetTop,
+                    behavior: "smooth"
+                });
+            }
+        });
+    }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const scrollTopBtn = document.getElementById("scrollTopBtn");
+
+    window.addEventListener("scroll", function () {
+        if (window.scrollY > 300) {
+            scrollTopBtn.classList.add("show");
+        } else {
+            scrollTopBtn.classList.remove("show");
+        }
+    });
+
+    scrollTopBtn.addEventListener("click", function () {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    });
+});
