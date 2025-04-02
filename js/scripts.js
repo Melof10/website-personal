2a8b4295ba6e6188bb8c840abe6acceea9a547ac
lc\melof <95fedemelo@gmail.com>
2025-04-02 04:35:21 -0300

    Modified html, css and js

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,12 @@
-/*!
-* Start Bootstrap - Personal v1.0.1 (https://startbootstrap.com/template-overviews/personal)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-personal/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+document.addEventListener("DOMContentLoaded", function () {
+    const navbar = document.querySelector(".navbar-custom");
+    const section = document.querySelector("section"); // Primer section de la página
+
+    window.addEventListener("scroll", function () {
+        if (window.scrollY > section.offsetTop - navbar.offsetHeight) {
+            navbar.classList.add("scrolled");
+        } else {
+            navbar.classList.remove("scrolled");
+        }
+    });
+});
